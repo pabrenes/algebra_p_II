b92e93262d545777526fd89f2efc3f5e0cbf474c
Pablo Brenes <pablobrenestec@gmail.com>
2017-06-03 20:56:15 -0600

    Interfaz súuuuuuuuuuper cuuuuuuuuute *-*

--- src/Interfaz/ControladorSistemasEcuaciones.java
+++ src/Interfaz/ControladorSistemasEcuaciones.java
@@ -7,17 +7,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.QuadCurve;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
@@ -29,10 +40,17 @@ public class ControladorSistemasEcuaciones implements Initializable {
     private int filas;
     private int columnas;
     private Matriz sistema;
+    private ArrayList<Group> parentesis;
 
-    private final String[] VARIABLES = {" X", " Y", " Z", "W", " S"};
+    private final String[] VARIABLES = {"X", "Y", "Z", "W", "S"};
     private final int ERROR_DIV_ZERO = 1;
     private final int ERROR_NON_NUME = 2;
+    private final int PARENTESIS_HGAP = 90;
+    private final double X_PRIMER_PARENTESIS = 528d;
+    private final double Y_PRIMER_PARENTESIS = 130d;
+    private final int CASILLA_ALTO = 50;
+    private final int CASILLA_ANCHO = 56;
+    private double LAST_Y;
 
     /**
      * Función que se autoejecuta en la creación de esta pantalla
@@ -44,6 +62,7 @@ public class ControladorSistemasEcuaciones implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         filas = 2;
         columnas = 2;
+        parentesis = new ArrayList<>();
         construirMatriz();
         configurarMenuFilas();
         configurarMenuColumnas();
@@ -56,6 +75,7 @@ public class ControladorSistemasEcuaciones implements Initializable {
      * Construye la matriz donde el usuario ingresa los datos
      */
     private void construirMatriz() {
+        lblSolucionHeader.setText("");
         gridPaneSistema.getChildren().clear();
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
@@ -130,7 +150,7 @@ public class ControladorSistemasEcuaciones implements Initializable {
      */
     private void eventoMenuFila(int fila) {
         filas = fila;
-        construirMatriz();
+        limpiar();
     }
 
     /**
@@ -140,7 +160,7 @@ public class ControladorSistemasEcuaciones implements Initializable {
      */
     private void eventoMenuColumna(int columna) {
         columnas = columna;
-        construirMatriz();
+        limpiar();
     }
 
     /**
@@ -149,15 +169,71 @@ public class ControladorSistemasEcuaciones implements Initializable {
      * @param solucion vector de dos dimensiones con las bases de la solución del sistema
      */
     private void construirSolucion(Fraccion[][] solucion) {
-        if (solucion.length == 0){
-            System.out.println("TODO CERO PAPU");
-        }
+        cleanParentesis();
         gridPaneSolucion.getChildren().clear();
-        for (int i = 0; i < solucion.length; i++) {
-            for (int j = 0; j < solucion[i].length; j++) {
-                gridPaneSolucion.add(construirEntradaSolucion(solucion, i, j), i, j);
+        lblSolucionHeader.setFont(new Font("Times New Roman", 18));
+        if (solucion.length == 0) {
+            construirSolucionTrivial();
+        } else {
+            lblSolucionHeader.setText(
+                    "El sistema posee soluciones infinitas. \n" +
+                            "La dimensión del espacio de solución es: " + solucion.length + ". \n" +
+                            "Una base para el espacio del sistema de solución es:");
+            for (int i = 0; i < solucion.length; i++) {
+                Group tmpGroup = crearParentesisVector(
+                        X_PRIMER_PARENTESIS + i * PARENTESIS_HGAP, Y_PRIMER_PARENTESIS);
+                panelPrincipal.getChildren().add(tmpGroup);
+                parentesis.add(tmpGroup);
+                for (int j = 0; j < solucion[i].length; j++) {
+                    gridPaneSolucion.add(construirEntradaSolucion(solucion, i, j), i, j);
+                }
             }
+            Group tmpGroup = crearComillas(solucion.length);
+            panelPrincipal.getChildren().add(tmpGroup);
+            parentesis.add(tmpGroup);
+            tmpGroup = parentesisConjunto();
+            panelPrincipal.getChildren().add(tmpGroup);
+            parentesis.add(tmpGroup);
         }
+    }
+
+    /**
+     * Solicita la construcción de la solución trivial para el sistema homogeneo
+     */
+    private void construirSolucionTrivial() {
+        lblSolucionHeader.setText(
+                "El sistema posee solución única. \n" +
+                        "Su solución es:");
+        for (int i = 0; i < columnas; i++) {
+            gridPaneSolucion.add(construirEntradaTrivial(VARIABLES[i]), 0, i);
+            gridPaneSolucion.add(construirEntradaTrivial("0"), 1, i);
+        }
+        Group parentesisVariables = crearParentesisVector(X_PRIMER_PARENTESIS, Y_PRIMER_PARENTESIS);
+        Group parentesisTriviales = crearParentesisVector(X_PRIMER_PARENTESIS + PARENTESIS_HGAP, Y_PRIMER_PARENTESIS);
+        Group igual = crearIgual();
+        panelPrincipal.getChildren().addAll(parentesisVariables, parentesisTriviales, igual);
+        parentesis.add(parentesisTriviales);
+        parentesis.add(parentesisVariables);
+        parentesis.add(igual);
+    }
+
+    /**
+     * Construye una entrada para la solución trivial
+     *
+     * @param variable incognita a la que se iguala
+     * @return VBox, con la entrada de la solución
+     */
+    private VBox construirEntradaTrivial(String variable) {
+        VBox contenedorVertical = new VBox();
+        contenedorVertical.setAlignment(Pos.CENTER);
+        contenedorVertical.setMinSize(52, 52);
+        contenedorVertical.setMaxSize(52, 52);
+        Label incognita = new Label();
+        incognita.setAlignment(Pos.CENTER);
+        incognita.setText(variable);
+        incognita.setFont(new Font("Times New Roman", 18));
+        contenedorVertical.getChildren().add(incognita);
+        return contenedorVertical;
     }
 
     /**
@@ -208,15 +284,15 @@ public class ControladorSistemasEcuaciones implements Initializable {
         TextField entradaDenominador = (TextField) vBox.getChildren().get(2);
         int numerador;
         int denominador;
-        try{
+        try {
             numerador = Integer.parseInt(entradaNumerador.getText());
             denominador = Integer.parseInt(entradaDenominador.getText());
-            if (denominador == 0){
+            if (denominador == 0) {
                 generateError(ERROR_DIV_ZERO);
                 return null;
             }
             return new Fraccion(numerador, denominador);
-        }catch(NumberFormatException nfe){
+        } catch (NumberFormatException nfe) {
             generateError(ERROR_NON_NUME);
         }
         return null;
@@ -273,6 +349,7 @@ public class ControladorSistemasEcuaciones implements Initializable {
     private void limpiar() {
         gridPaneSistema.getChildren().clear();
         construirMatriz();
+        cleanParentesis();
         gridPaneSolucion.getChildren().clear();
     }
 
@@ -294,6 +371,255 @@ public class ControladorSistemasEcuaciones implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Crea un paréntesis para almacenar el vector
+     *
+     * @param X Inicio del paréntesis en X coordenada del Panel
+     * @param Y Inicio del paréntesis en y coordenada del Panel
+     * @return Group, conjunto de Lines y CubicCurves que forman el parénteis
+     */
+    private Group crearParentesisVector(Double X, Double Y) {
+
+        QuadCurve curvaSuperiorIzquierda = new QuadCurve(
+                X, Y + 30,
+                X, Y + 10,
+                X + 10, Y);
+        curvaSuperiorIzquierda.setStroke(Color.BLACK);
+        curvaSuperiorIzquierda.setFill(null);
+        curvaSuperiorIzquierda.setStrokeWidth(2);
+
+        QuadCurve curvaSuperiorDerecha = new QuadCurve(
+                X + CASILLA_ANCHO, Y + 30,
+                X + CASILLA_ANCHO, Y + 10,
+                X + CASILLA_ANCHO - 10, Y
+        );
+        curvaSuperiorDerecha.setStroke(Color.BLACK);
+        curvaSuperiorDerecha.setFill(null);
+        curvaSuperiorDerecha.setStrokeWidth(2);
+
+        Line lineIzquierda = new Line();
+        lineIzquierda.setStartX(X);
+        lineIzquierda.setEndX(X);
+        lineIzquierda.setStartY(Y + 32);
+
+        Line lineDerecha = new Line();
+        lineDerecha.setStartX(X + CASILLA_ANCHO);
+        lineDerecha.setEndX(X + CASILLA_ANCHO);
+        lineDerecha.setStartY(Y + 32);
+
+        Y += (columnas * CASILLA_ALTO) + -5 * (columnas - 5);
+        LAST_Y = Y;
+
+        lineIzquierda.setEndY(Y - 32);
+        lineIzquierda.setStroke(Color.BLACK);
+        lineIzquierda.setStrokeWidth(2);
+
+        lineDerecha.setEndY(Y - 32);
+        lineIzquierda.setStroke(Color.BLACK);
+        lineDerecha.setStrokeWidth(2);
+
+        QuadCurve curvaInferiorIzquierda = new QuadCurve(
+                X, Y - 30,
+                X, Y - 10,
+                X + 10, Y);
+        curvaInferiorIzquierda.setStroke(Color.BLACK);
+        curvaInferiorIzquierda.setFill(null);
+        curvaInferiorIzquierda.setStrokeWidth(2);
+
+        QuadCurve curvaInferiorDerecha = new QuadCurve(
+                X + CASILLA_ANCHO, Y - 30,
+                X + CASILLA_ANCHO, Y - 10,
+                X + CASILLA_ANCHO - 10, Y
+        );
+        curvaInferiorDerecha.setStroke(Color.BLACK);
+        curvaInferiorDerecha.setFill(null);
+        curvaInferiorDerecha.setStrokeWidth(2);
+        return new Group(
+                curvaSuperiorIzquierda, lineIzquierda, curvaInferiorIzquierda,
+                curvaSuperiorDerecha, lineDerecha, curvaInferiorDerecha);
+    }
+
+    /**
+     * Crea los paréntesis que albergan al conjunto de solución
+     *
+     * @return Grupo, con los shapes necesarios para crear el conjunto de solución
+     */
+    private Group parentesisConjunto() {
+        Group group = new Group();
+        double XDES = 12;
+        double YDES = 10;
+        double XEND = (PARENTESIS_HGAP * (columnas - 1)) + CASILLA_ANCHO;
+        double X = X_PRIMER_PARENTESIS;
+        double Y = Y_PRIMER_PARENTESIS;
+
+        QuadCurve curvaSuperiorIzquierda = new QuadCurve(
+                X - XDES, Y + 30 - YDES,
+                X - XDES, Y - YDES,
+                X - XDES + 10, Y - YDES);
+        curvaSuperiorIzquierda.setStroke(Color.BLACK);
+        curvaSuperiorIzquierda.setFill(null);
+        curvaSuperiorIzquierda.setStrokeWidth(2);
+
+        QuadCurve curvaSuperiorDerecha = new QuadCurve(
+                X + XEND + XDES, Y + 30 - YDES,
+                X + XEND + XDES, Y - YDES,
+                X + XEND + XDES - 10, Y - YDES);
+        curvaSuperiorDerecha.setStroke(Color.BLACK);
+        curvaSuperiorDerecha.setFill(null);
+        curvaSuperiorDerecha.setStrokeWidth(2);
+
+        Line lineaSuperiorIzquierda = new Line();
+        lineaSuperiorIzquierda.setStartX(X - XDES);
+        lineaSuperiorIzquierda.setStartY(Y + 32 - YDES);
+        lineaSuperiorIzquierda.setEndX(X - XDES);
+
+        Line lineaSuperiorDerecha = new Line();
+        lineaSuperiorDerecha.setStartX(X + XEND + XDES);
+        lineaSuperiorDerecha.setStartY(Y + 32 - YDES);
+        lineaSuperiorDerecha.setEndX(X + XEND + XDES);
+
+        Y += ((columnas * CASILLA_ALTO) + -5 * (columnas - 5)) / 2 - YDES;
+
+        lineaSuperiorIzquierda.setEndY(Y);
+        lineaSuperiorIzquierda.setStroke(Color.BLACK);
+        lineaSuperiorIzquierda.setStrokeWidth(2);
+
+        lineaSuperiorDerecha.setEndY(Y);
+        lineaSuperiorDerecha.setStroke(Color.BLACK);
+        lineaSuperiorDerecha.setStrokeWidth(2);
+
+        QuadCurve curvaMediaSuperiorIzquierda = new QuadCurve(
+                X - XDES, Y,
+                X - XDES, Y + YDES,
+                X - XDES - XDES, Y + YDES);
+        curvaMediaSuperiorIzquierda.setStroke(Color.BLACK);
+        curvaMediaSuperiorIzquierda.setFill(null);
+        curvaMediaSuperiorIzquierda.setStrokeWidth(2);
+
+        QuadCurve curvaMediaSuperiorDerecha = new QuadCurve(
+                X + XEND + XDES, Y,
+                X + XEND + XDES, Y + YDES,
+                X + XEND + XDES + XDES, Y + YDES);
+        curvaMediaSuperiorDerecha.setStroke(Color.BLACK);
+        curvaMediaSuperiorDerecha.setFill(null);
+        curvaMediaSuperiorDerecha.setStrokeWidth(2);
+
+        QuadCurve curvaMediaInferiorIzquierda = new QuadCurve(
+                X - XDES - XDES, Y + YDES,
+                X - XDES, Y + YDES,
+                X - XDES, Y + YDES + YDES);
+        curvaMediaInferiorIzquierda.setStroke(Color.BLACK);
+        curvaMediaInferiorIzquierda.setFill(null);
+        curvaMediaInferiorIzquierda.setStrokeWidth(2);
+
+        QuadCurve curvaMediaInferiorDerecha = new QuadCurve(
+                X + XEND + XDES + XDES, Y + YDES,
+                X + XEND + XDES, Y + YDES,
+                X + XEND + XDES, Y + YDES + YDES);
+        curvaMediaInferiorDerecha.setStroke(Color.BLACK);
+        curvaMediaInferiorDerecha.setFill(null);
+        curvaMediaInferiorDerecha.setStrokeWidth(2);
+
+        Line lineaInferiorIzquierda = new Line();
+        lineaInferiorIzquierda.setStartX(X - XDES);
+        lineaInferiorIzquierda.setStartY(Y + 32 - YDES);
+        lineaInferiorIzquierda.setEndX(X - XDES);
+
+        Line lineaInferiorDerecha = new Line();
+        lineaInferiorDerecha.setStartX(X + XEND + XDES);
+        lineaInferiorDerecha.setStartY(Y + 32 - YDES);
+        lineaInferiorDerecha.setEndX(X + XEND + XDES);
+
+        Y += ((columnas * CASILLA_ALTO) + -5 * (columnas - 5)) / 2 - YDES;
+
+        lineaInferiorIzquierda.setEndY(Y);
+        lineaInferiorIzquierda.setStroke(Color.BLACK);
+        lineaInferiorIzquierda.setStrokeWidth(2);
+
+        lineaInferiorDerecha.setEndY(Y);
+        lineaInferiorDerecha.setStroke(Color.BLACK);
+        lineaInferiorDerecha.setStrokeWidth(2);
+
+        QuadCurve curvaInferiorIzquierda = new QuadCurve(
+                X - XDES, Y,
+                X - XDES, Y + 21 + YDES,
+                X - XDES + 10, Y + 21 + YDES);
+        curvaInferiorIzquierda.setStroke(Color.BLACK);
+        curvaInferiorIzquierda.setFill(null);
+        curvaInferiorIzquierda.setStrokeWidth(2);
+
+        QuadCurve curvaInferiorDerecha = new QuadCurve(
+                X + XEND + XDES, Y,
+                X + XEND + XDES, Y + 21 + YDES,
+                X + XEND + XDES - 10, Y + 21 + YDES);
+        curvaInferiorDerecha.setStroke(Color.BLACK);
+        curvaInferiorDerecha.setFill(null);
+        curvaInferiorDerecha.setStrokeWidth(2);
+
+
+        group.getChildren().addAll(
+                curvaSuperiorIzquierda, lineaSuperiorIzquierda, curvaMediaSuperiorIzquierda, curvaMediaInferiorIzquierda, lineaInferiorIzquierda, curvaInferiorIzquierda,
+                curvaSuperiorDerecha, lineaSuperiorDerecha, curvaMediaSuperiorDerecha, curvaMediaInferiorDerecha, lineaInferiorDerecha, curvaInferiorDerecha);
+        return group;
+    }
+
+    /**
+     * Crea las comillas que se paran los elementos del espacio de solución
+     *
+     * @return Retorna un grupo con las comillas asignadas
+     */
+    private Group crearComillas(int dimension) {
+        Group grupo = new Group();
+        double relativeY = (LAST_Y - Y_PRIMER_PARENTESIS) / 2 + Y_PRIMER_PARENTESIS;
+        double tmp = PARENTESIS_HGAP / 2;
+        double relativeX = tmp / 2 + tmp + X_PRIMER_PARENTESIS;
+        for (int i = 0; i < dimension - 1; i++) {
+            double X = relativeX + (PARENTESIS_HGAP * i) + 5;
+            Circle punto = new Circle(X, relativeY, 2);
+            punto.setStroke(Color.BLACK);
+            QuadCurve cola = new QuadCurve(
+                    X - 1.5, relativeY + 6,
+                    X + 1.5, relativeY + 6,
+                    X + 1.5, relativeY
+            );
+            cola.setStroke(Color.BLACK);
+            cola.setFill(null);
+            cola.setStrokeWidth(2);
+            grupo.getChildren().addAll(punto, cola);
+        }
+        return grupo;
+    }
+
+    /**
+     * Crea un signo igual para la solución trivial
+     *
+     * @return Grupo con dos iguales creados
+     */
+    private Group crearIgual() {
+        Group group = new Group();
+        double relativeY = (LAST_Y - Y_PRIMER_PARENTESIS) / 2 + Y_PRIMER_PARENTESIS;
+        double tmp = PARENTESIS_HGAP / 2;
+        double relativeX = tmp / 2 + tmp + X_PRIMER_PARENTESIS + 5;
+        Line lineSuperior =
+                new Line(relativeX - 5, relativeY - 4, relativeX + 6, relativeY - 4);
+        lineSuperior.setStroke(Color.BLACK);
+        lineSuperior.setStrokeWidth(3);
+        Line lineInferior =
+                new Line(relativeX - 5, relativeY + 4, relativeX + 6, relativeY + 4);
+        lineInferior.setStroke(Color.BLACK);
+        lineInferior.setStrokeWidth(3);
+        group.getChildren().addAll(lineInferior, lineSuperior);
+        return group;
+    }
+
+    /**
+     * Eliminar los paréntesis actuales en la gráfica y limpia el ArrayList que los contiene
+     */
+    private void cleanParentesis() {
+        panelPrincipal.getChildren().removeAll(parentesis);
+        parentesis.clear();
     }
 
     //UI
@@ -319,6 +645,10 @@ public class ControladorSistemasEcuaciones implements Initializable {
     public Button btnLimpiar;
     @FXML
     public Button btnVolver;
+    @FXML
+    public Pane panelPrincipal;
+    @FXML
+    public Label lblSolucionHeader;
     @FXML
     public GridPane gridPaneSistema;
     @FXML
